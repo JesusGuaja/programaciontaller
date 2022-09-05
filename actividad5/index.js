8e86c786b0b27cc1e16c513ceadae91209408923
JesusGuaja <jesusguajardo080@gmail.com>
2022-09-05 17:02:06 -0300

    ultimoscambios

---
+++ actividad5/index.js
@@ -0,0 +1,49 @@
+
+let criptos = ['Bitcoin', 'Ethereum’', 'Tether', 'BNB' , 'Cardano' , 'Dai' , 'Helium', 'Shiba Inu' , 'Tron' , 'Cronos','Solana'];
+
+let longitud = criptos.length;
+console.log('Longitud del arreglo = ' + longitud);
+
+criptos.unshift('Stellar'); 
+console.log(criptos);
+
+let indice = criptos.indexOf('Shiba Inu');
+let eliminados = criptos.splice(indice, 2);
+console.log(criptos);
+console.log(`Elementos eliminados: ${eliminados}`);
+
+criptos.push('Gate');
+console.log(criptos);
+
+function imprimir() { 
+    let parrafo = document.getElementById("parrafo");
+    let longitud = criptos.length;
+    let ultimo = criptos[longitud-1];
+    let indice3 = criptos.indexOf('Helium');
+
+    parrafo.innerHTML = `
+    <b>Longitud</b> = ${longitud} <br>
+    <b>Ultimo elemento</b> = ${ultimo} <br>
+    <b>Elemento seleccionado</b> = ${criptos[indice3]}`  
+}
+
+document.getElementById("imprimir_btn").addEventListener("click", imprimir);
+
+
+
+function imprimir_lista() {
+    let lista = document.getElementById("lista");
+
+    let items = []
+    criptos.forEach( (element, index) => {
+        //console.log((index+1) + '- '+element);
+        let item = `
+        <li><b>${element}</b></li>
+        `
+        items.push(item)
+    }); 
+
+    document.getElementById("lista").innerHTML = items.join("");
+}
+
+document.getElementById("lista_btn").addEventListener("click", imprimir_lista);
--- repaso/arreglos/index.js
+++ repaso/arreglos/index.js
@@ -12,7 +12,7 @@ let ultimo = lenguajes[longitud-1];
 //recorrer un arreglo
 lenguajes.forEach( (element, index) => {
     console.log((index+1) + '- '+element);
-}) 
+}); 
 
 //agregar un elemento al final del arreglo
 lenguajes.push('Php');
@@ -28,3 +28,23 @@ console.log(lenguajes);
 
 //como eliminar el primero elemento de un arreglo
 lenguajes.shift();
+console.log(lenguajes);
+
+//como encontrar el indice de un elemento y mostrarlo posteriormente
+let indice = lenguajes.indexOf('JavaScript');
+console.log(lenguajes[indice]);
+
+//eliminar cualquier elemento del arreglo
+let indice2 = lenguajes.indexOf('C++')
+let eliminados = lenguajes.splice(indice2, 2);
+console.log(lenguajes);
+console.log(eliminados);
+
+//agregar un elemento dentro de un arreglo
+lenguajes.splice(indice, 0, 'Visual Basic');
+console.log(lenguajes); 
+
+//como copiar un arreglo
+let copia = lenguajes.slice();
+copia.push('Ruby');
+console.log(copia);
